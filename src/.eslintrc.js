f7e9d6dfc8b910fb6e98c6d74600c909fe0f0f4f
Renzo Simonetto <simonetto@gmail.com>
2017-05-03 20:18:37 +0100

    [impl] First draft

---
+++ src/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'angular'
+  ],
+  rules: {
+    'angular/no-service-method': 0,
+    'linebreak-style': 1,
+    'indent': 1,
+    'spaced-comment': 1
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import angular from 'angular';
+/*
+import {techsModule} from './app/techs/index';
+import 'angular-ui-router';
+import routesConfig from './routes';
+
+import {main} from './app/main';
+import {header} from './app/header';
+import {title} from './app/title';
+import {footer} from './app/footer';
+
+import './index.scss';
+
+angular
+  .module('app', [techsModule, 'ui.router'])
+  .config(routesConfig)
+  .component('app', main)
+  .component('fountainHeader', header)
+  .component('fountainTitle', title)
+  .component('fountainFooter', footer);*/
+
+  import 'angular-ui-router';
+  import routesConfig from './routes';
+
+  import {main} from './app/main/main';
+
+  import './index.scss';
+
+  angular
+    .module('adbu', ['ui.router'])
+    .config(routesConfig)
+    .component('adbu', main);
